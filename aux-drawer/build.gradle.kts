0b2766f2a608a0975f45c65d1b74aec6587f1dcd
DemonstrusDimedrolov <dimaklusin298@gmail.com>
2025-04-18 12:21:02 +0900

    v1.0.2

--- aux-drawer/build.gradle.kts
+++ aux-drawer/build.gradle.kts
@@ -58,7 +58,7 @@ publishing {
         create<MavenPublication>("release") {
             groupId = "com.github.DemonstrusDimedrolov"
             artifactId = "auxdrawer"
-            version = "1.0.1"
+            version = "1.0.2"
 
             afterEvaluate {
                 from(components["release"])
